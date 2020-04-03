99433efbf2bbf67642ca7c359e9d4e327129ffb9
Diane Crame <dcrame@icloud.com>
2020-04-03 16:40:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,24 +7,8 @@ class Anagram
     end
     
     def match(array)
-      arr_split = array.map {|word| word.split("")}
-      word_split = @word.split("")
-      new_arr = []
-      words = arr_split.collect do |word| 
-        if word.size == word_split.size
-          new_arr << word 
-        end  
-      end
-      result = new_arr.collect do |word|
-        (word & word_split).flat_map {|n|[n]*[word.count(n), word_split.count(n)].min}.join('')
-      end
-      results = []
-      word_split_size = word_split.join("").size
-      result.map do |each| 
-        if each.size == word_split_size 
-          results << each
+        array.select do |word|
+          word.split("").sort == @word.split("").sort
         end
-      end
-      results
     end
 end
