350a93d742d7e6de4dc5f8df4c001e20efa6dcb6
Diane Crame <dcrame@icloud.com>
2020-04-03 15:59:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+    def initialize(word)
+      @word = word
+    end
+    
+    def match(array)
+      split_arr = array.map {|word| word.split("")}
+     
+      word = @word.split("")
+      new_arr = []
+      split_arr.select do |arr| 
+        new_arr << arr & word == arr && arr.length == word.length
+        new_arr
+      end
+      new_arr.map {|word| word.join("")}
+    end
+end
